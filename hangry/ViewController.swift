fa28cb0680f899db57b4f31dd346859141a7234a
Winnie Wen <wiwen@berkeley.edu>
2015-12-07 21:54:19 -0800

    added polylines

--- hangry/ViewController.swift
+++ hangry/ViewController.swift
@@ -10,55 +10,125 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+
     
     
     
-    let locationManager = CLLocationManager()
+    var locationManager: CLLocationManager!
     let searchRadius: CLLocationDistance = 2000
     
     let initialLocation = CLLocation(latitude: 49.140838, longitude: -123.127886)
+    var points: [CLLocationCoordinate2D] = [CLLocationCoordinate2D]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        // Current Location
-        self.locationManager.delegate = self
-        self.locationManager.requestWhenInUseAuthorization()
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //For Location 1
+        let location1 = CLLocationCoordinate2D(
+            latitude: 51.481188400000010000,
+            longitude: -0.190209099999947280
+        )
+        self.mapView.delegate = self
         
-        //self.locationManager.startUpdatingLocation()
-        self.mapView.showsUserLocation = true
+        let annotation1: CustomPointAnnotation = CustomPointAnnotation()
+        annotation1.coordinate = location1;
+        annotation1.title = "Hangry Bird"
+        annotation1.subtitle = "Hangry Bird's location"
         
+        let span = MKCoordinateSpanMake(0.15, 0.15)
+        
+        let region1 = MKCoordinateRegion(center: location1, span: span)
+        mapView.setRegion(region1, animated: true)
+        mapView.addAnnotation(annotation1)
+        
+        //For Location 2
+        let location2 = CLLocationCoordinate2D(
+            latitude: 51.554947700000010000,
+            longitude: -0.108558899999934510
+        )
+        
+        let annotation2: CustomPointAnnotation = CustomPointAnnotation()
+        annotation2.coordinate = location2;
+        annotation2.title = "Arsenal"
+        annotation2.subtitle = "Arsenal"
+        
+        let region2 = MKCoordinateRegion(center: location1, span: span)
+        mapView.setRegion(region2, animated: true)
+        mapView.addAnnotation(annotation2)
+        
+        var locations = [CLLocation(latitude: 51.481188400000010000, longitude: -0.190209099999947280), CLLocation(latitude: 51.554947700000010000,longitude:  -0.108558899999934510)]
     
-        //mapView.setRegion(region, animated: true)
-
-    
-        mapView.delegate = self
+        var coordinates = locations.map {
+            location in
+            return location.coordinate
+        }
         
-    
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(initialLocation.coordinate, searchRadius * 2.0, searchRadius * 2.0)
+        //var polyline = MKPolyline(coordinates: &coordinates, count: locations.count)
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location1, searchRadius * 2.0, searchRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
-        mapView.delegate = self
         searchInMap()
-        addBoundry()
+        
+        //var points: [CLLocationCoordinate2D] = [CLLocationCoordinate2D]()
+        
+        var polyline = MKPolyline(coordinates:&points, count: points.count)
+        mapView.addOverlay(polyline)
+        
 
     }
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if overlay is MKPolyline {
+            var polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = UIColor.blueColor()
+            polylineRenderer.lineWidth = 5
+            return polylineRenderer
+            
+        }
+        
+        return nil
+    }
+    
 
+    func searchInMap() {
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = "Restaurants"
+        request.region = mapView.region
+        
+        let search: MKLocalSearch = MKLocalSearch(request: request)
+        
+        search.startWithCompletionHandler {
+            response, error in guard let response = response
+                else {
+                    print("Error")
+                    return
+            }
+            
+            for item in response.mapItems {
+                self.addPinToMapView(item.name!, latitude: item.placemark.location!.coordinate.latitude, longitude: item.placemark.location!.coordinate.longitude)
+                
+            }
+        }
+    }
+    
+    
+    
     func addPinToMapView(title: String, latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
         let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-
+        
         var pointAnnotation: CustomPointAnnotation = CustomPointAnnotation()
         pointAnnotation.coordinate = location
         pointAnnotation.title = title
         
-        pointAnnotation.pinCustomImageName = "food.png"
+        //pointAnnotation.pinCustomImageName = "food.png"
+        points.append(location)
         
         self.mapView.addAnnotation(pointAnnotation)
         self.mapView?.selectAnnotation(pointAnnotation, animated: true)
+        var polyline = MKPolyline(coordinates:&points, count: points.count)
+        mapView.addOverlay(polyline)
         
         
     }
@@ -76,48 +146,20 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             else {
                 anView!.annotation = annotation
             }
-            
-            let cpa = annotation as! CustomPointAnnotation
-            anView!.image = UIImage(named:cpa.pinCustomImageName)
-            
+            if (anView!.annotation!.title! == "Hangry Bird") {
+
+                anView!.image = UIImage(named:"angry-bird-icon.png")
+                //anView!.draggable = true
+            }
+            else {
+                anView!.image = UIImage(named:"ice_cream.png")
+                
+            }
             return anView
     }
-    func addBoundry()
-    {
-        var points=[CLLocationCoordinate2DMake(49.142677,  -123.135139),CLLocationCoordinate2DMake(49.142730, -123.125794),CLLocationCoordinate2DMake(49.140874, -123.125805),CLLocationCoordinate2DMake(49.140885, -123.135214)]
-        
-        let polygon = MKPolygon(coordinates: &points, count: points.count)
-        
-        mapView.addOverlay(polygon)
-    }
     
-
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            
-            // Dispose of any resources that can be recreated.
-    
-        }
-
-
-    func searchInMap() {
-        let request = MKLocalSearchRequest()
-        request.naturalLanguageQuery = "Restaurants"
-        request.region = mapView.region
-
-        let search: MKLocalSearch = MKLocalSearch(request: request)
-        
-        search.startWithCompletionHandler {
-            response, error in guard let response = response
-            else {
-                print("Error")
-                return
-            }
-            
-            for item in response.mapItems {
-                self.addPinToMapView(item.name!, latitude: item.placemark.location!.coordinate.latitude, longitude: item.placemark.location!.coordinate.longitude)
-            
-            }
-        }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 }
